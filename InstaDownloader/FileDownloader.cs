5299654a72281008f8110771f9ccfff6a444a566
Артем Крылов <kav128@outlook.com>
2018-01-24 21:13:52 +0300

    add FileDownloader class

---
+++ InstaDownloader/FileDownloader.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace InstaDownloader
+{
+    static class FileDownloader
+    {
+        public static void Download(string URL, string Dest)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(URL, Dest);
+            }
+        }
+    }
+}
