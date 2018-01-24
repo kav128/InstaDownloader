812e52db4b1345dcaef189cc03d41384e75e1a19
Артем Крылов <kav128@outlook.com>
2018-01-24 21:02:20 +0300

    first commit

---
+++ InstaDownloader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
